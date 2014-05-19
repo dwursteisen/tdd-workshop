2bbead5734ebe0d2fd35a075fda4fb9951093c1f
David Wursteisen <david.wursteisen+git@gmail.com>
2014-05-19 21:34:00 +0200

    Ajout d'un commentaire pour l'exercice

--- 01-TDD/src/test/java/tdd/FooBarQixTest.java
+++ 01-TDD/src/test/java/tdd/FooBarQixTest.java
@@ -11,6 +11,29 @@ import static org.assertj.core.api.Assertions.*;
  */
 public class FooBarQixTest {
 
+
+
+   /**
+
+   Les premières valeurs de l'exercice doivent être :
+
+   1
+   2
+   FooFoo
+   4
+   BarBar
+   Foo
+   QixQix
+   8
+   Foo
+   Bar
+   Foo
+   FooBarBar
+   FooQix
+   FooFooFoo
+   FooBar
+   BarFoo
+     **/
     @Test
     public void should_be_true() {
         assertThat(true).isTrue(); // yeah, really !
