7aecc6abb884687b81f1c1f1398e3327a093b630
David Wursteisen <david.wursteisen+git@gmail.com>
2014-05-22 10:57:44 +0200

    Correction des valeurs de FooBarQix

--- 01-TDD/src/test/java/tdd/FooBarQixTest.java
+++ 01-TDD/src/test/java/tdd/FooBarQixTest.java
@@ -18,23 +18,35 @@ public class FooBarQixTest {
 
    Les premières valeurs de l'exercice doivent être :
 
-   1
-   2
-   FooFoo
-   4
-   BarBar
-   Foo
-   QixQix
-   8
-   Foo
-   Bar
-   Foo
-   FooBarBar
-   FooQix
-   FooFooFoo
-   FooBar
-   BarFoo
+    1
+    2
+    FooFoo
+    4
+    BarBar
+    Foo
+    QixQix
+    8
+    Foo
+    Bar
+    11
+    Foo
+    Foo
+    Qix
+    FooBarBar
+    16
+    Qix
+    Foo
+    19
+    Bar
+    FooQix
+    22
+    Foo
+    Foo
+    BarBar
+    26
+    FooQix
      **/
+
     @Test
     public void should_be_true() {
         Assertions.assertThat(true).isTrue();
